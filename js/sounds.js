abbf5c20f6f6eecfe462c82936923924c8b41cfc
Gabriel Klavans <gabeklav@bu.edu>
2020-08-04 19:56:52 -0400

    Pause/play with volume

--- js/sounds.js
+++ js/sounds.js
@@ -1,45 +1,43 @@
-
 // create web audio api context
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-var oscillator, gainNode;
+const masterGain = audioCtx.createGain();
 
 var isPlaying = false;
 
-function playTone(freq) {
-    // Create Oscillator node
-    oscillator = audioCtx.createOscillator();
-    oscillator.type = 'sine';
-    oscillator.frequency.setValueAtTime(freq, audioCtx.currentTime); // value in hertz
+// Create Oscillator node
+var osc1 = audioCtx.createOscillator();
+osc1.type = 'sine';
+osc1.frequency.setValueAtTime(440, audioCtx.currentTime); // value in hertz
 
-    // Create Gain node
-    gainNode = audioCtx.createGain();
-    // gainNode.gain.setValueAtTime(0, audioCtx.currentTime); // Start quiet
+// Create Gain node
+var gain1 = audioCtx.createGain();
+// gainNode.gain.setValueAtTime(0, audioCtx.currentTime); // Start quiet
 
-    // Da hook up
-    oscillator.connect(gainNode).connect(audioCtx.destination);
+// Da hook up
+osc1.connect(gain1).connect(masterGain).connect(audioCtx.destination);
 
-    oscillator.start();
-    isPlaying = true;
-}
+osc1.start();
+audioCtx.suspend();
+isPlaying = true;
 
 function stopTone() {
-    gainNode ? gainNode.gain.exponentialRampToValueAtTime(0.00001, audioCtx.currentTime + 0.04) : console.log("durr") // do nothin
+    gain1 ? gain1.gain.exponentialRampToValueAtTime(0.00001, audioCtx.currentTime + 0.04) : console.log("durr") // do nothin
     isPlaying = false;
 }
 
-function buttonHandler(freq) {
-    if (isPlaying) {
-        stopTone();
-        console.log("Stopping!");
-    } else {
-        playTone(freq);
-        console.log("Starting!");
-    }
-}
-
 const volumeControl = document.querySelector('#volume');
-
-volumeControl.addEventListener('input', function() {
-    gainNode.gain.value = this.value;
+volumeControl.addEventListener('input', function () {
+    masterGain.gain.value = this.value;
 }, false);
+
+const playToggle = document.getElementById("playToggle");
+playToggle.addEventListener("click", () => {
+    if (audioCtx.state === "running") {
+        audioCtx.suspend();
+        volumeControl.disabled = true;
+    } else if (audioCtx.state === "suspended") {
+        audioCtx.resume();
+        volumeControl.disabled = false;
+    }
+})
